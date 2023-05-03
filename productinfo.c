9a19f66dea88a57b9a7b1456daf36dcb0b159486
Andrew Long <54878469+andyrlong@users.noreply.github.com>
2023-05-02 23:02:51 -0400

    Add files via upload

---
+++ productinfo.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(void) 
+{
+    int itemNum, month, day, year;
+    float unitPrice;
+    
+    printf("Enter item number: ");
+    scanf("%d", &itemNum);
+    
+    printf("Enter unit price: ");
+    scanf("%f", &unitPrice);
+
+    printf("Enter purchase date (mm/dd/yyyy): ");
+    scanf("%d/%d/%d", &month, &day, &year);
+
+
+    
+    printf("Item\tUnit\tPurchase\n\tPrice\tDate\n");
+    printf("%d\t$%.2f\t%d/%d/%d", itemNum, unitPrice, month, day, year);
+
+return 0;
+}
