7aa699743a2c1b75afe6dd4ec1389db43c0d18b3
Andrew Long <54878469+andyrlong@users.noreply.github.com>
2023-05-02 23:03:09 -0400

    Update productinfo.c

--- productinfo.c
+++ productinfo.c
@@ -13,8 +13,6 @@ int main(void)
 
     printf("Enter purchase date (mm/dd/yyyy): ");
     scanf("%d/%d/%d", &month, &day, &year);
-
-
     
     printf("Item\tUnit\tPurchase\n\tPrice\tDate\n");
     printf("%d\t$%.2f\t%d/%d/%d", itemNum, unitPrice, month, day, year);
